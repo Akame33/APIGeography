0c8fd5bed0e1e7518fcbeaf00a94665562287324
LisonM333 <li.marvin@laposte.net>
2024-12-21 13:52:38 +0100

    page info (html, css, js)

--- info.js
+++ info.js
@@ -1,10 +1,13 @@
 
 const cardContainer = document.getElementById('container');
 
-async function fetchData() {
+async function fetchData(triFunction) {
   try {
     const response = await fetch('https://restcountries.com/v3.1/all');
-    const data = await response.json();
+    let data = await response.json();
+    if (triFunction){
+        data = triFunction(data);
+    }
     createCards(data);
   } catch (error) {
     console.error('Erreur lors de la récupération des données :', error);
@@ -12,20 +15,56 @@ async function fetchData() {
 }
 
 function createCards(data) {
-  data.forEach(country => {
-    const card = document.createElement('div');
-    card.className = 'card';
-    card.innerHTML = `
-        <img src="${country.flag.png}" alt="Flag of ${country.name.common}">
-        <p><strong>Country :</strong> ${country.name.common}</p>
-        <p><strong>Capital:</strong> ${country.capital ? country.capital[0] : 'N/A'}</p>
-        <p><strong>Region :</strong> ${country.region}</p>
-        <p><strong>Population :</strong> ${country.population.toLocaleString()} inhabitant</p>
-    `;
-    cardContainer.appendChild(card);
-  });
+    const cardContainer = document.getElementById('container');
+    cardContainer.innerHTML = '';
+    data.forEach(country => {
+        const card = document.createElement('div');
+        card.className = 'card';
+        card.innerHTML = `
+            <img src="${country.flags.png}" alt="Flag of ${country.name.common}">
+            <p class="clearcolor"><strong>Country :</strong> ${country.name.common}</p>
+            <p class="clearcolor"><strong>Capital:</strong> ${country.capital ? country.capital[0] : 'N/A'}</p>
+            <p class="clearcolor"><strong>Region :</strong> ${country.region}</p>
+            <p class="clearcolor"><strong>Population :</strong> ${country.population.toLocaleString()} inhabitant</p>
+        `;
+        cardContainer.appendChild(card);
+    });
 }
 
-fetchData();
-console.log(data);
+function Alphaorder(data){
+    return data.sort((a, b) => {
+    return a.name.common.localeCompare(b.name.common);
+})}
 
+fetchData(Alphaorder);
+
+
+
+function Contorder(data){
+    data.sort((a, b) => {
+    return a.name.common.localeCompare(b.name.common);
+});
+    return data.sort((a, b) => {
+    return a.region.localeCompare(b.region);
+})}
+
+function Popdescorder(data){
+    return data.sort((a, b) => b.population - a.population);
+}
+
+function Popascorder(data){
+    return data.sort((a, b) => a.population - b.population);
+}
+
+document.querySelector('.alpha').addEventListener('click', () => {
+    fetchData(Alphaorder);
+});
+document.querySelector('.cont').addEventListener('click', () => {
+    fetchData(Contorder);
+});
+document.querySelector('.popdesc').addEventListener('click', () => {
+    fetchData(Popdescorder);
+});
+document.querySelector('.popasc').addEventListener('click', () => {
+    fetchData(Popascorder);
+});
